6b4be753f222b1813b148131554146860df83f00
Chakrit Wichian <service@chakrit.net>
2015-03-23 04:31:06 +0700

    Don't run *_test.go files.

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-cat input.txt | go run *.go | diff - output.txt
+FILES="chars.go context.go main.go parse.go stack.go queue.go lex.go compile.go nodes.go tokens.go"
+cat input.txt | go run $FILES | diff - output.txt
 
